7f83b593b76119f0e6cc80a58111627f852fb5e5
Nija Unmesh <nijaunmesh123@gmail.com>
2025-10-20 11:12:40 +0530

    problemsolved

--- resumeBuilder-fe/src/pages/Home.jsx
+++ resumeBuilder-fe/src/pages/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
                         <h3 className='text-center p-5'>Tools</h3>
                         <div  class="container">  
                             <div class="row">
-                                <div class="col-6">
+                                <div class="col-md-6 col-12">
                                     <h3>Resume</h3>
                                     <h6>Create unlimited new resumes and easily edit them afterwards.</h6>
                                     <h3>Cover Letters</h3>
@@ -35,7 +35,7 @@ const Home = () => {
                                     <h3>Applications</h3>
                                     <h6>Effortlessly manage and track your job applications in an organized manner.</h6>
                                 </div>
-                                <div class="col-6 text-center">
+                                <div class="col-md-6 col-12 text-center">
                                     <img width="70%" alt="" src="https://cdn-images.zety.com/images/zety/landings/builder/resume-builder-jumbotron-image@3x.png"></img>
                                 </div>
                             </div>
@@ -45,15 +45,16 @@ const Home = () => {
 
                     </div>
                     
-                    <div className='height'>
+                    
 
-                    </div>
+                </div>
 
+                
                     <div className='stc'>
                         <h3 className='text-center p-5'>Testimony</h3>
                         <div  class="container">  
                             <div class="row">
-                                <div class="col-6">
+                                <div class="col-md-6 col-12">
                                     <h3 className='lines'>Trusted by professionals worldwide.</h3>
                                    <p className="lines">At LiveCareer, we don't just help you create résumés — we help you land the job. Whether you're a seasoned professional or just starting out, our tools are designed to get results.</p>
 
@@ -61,7 +62,7 @@ const Home = () => {
 
 <p className="lines">Join thousands of job-seekers who’ve fast-tracked their careers with a résumé that truly stands out.</p>
                                 </div>
-                                <div class="col-6 text-center">
+                                <div class="col-md-6 col-12 text-center">
                                    <div class="row">
                                         <div className="col-3">
                                             <div className='mb-3 '><img src="https://assets.livecareer.co.uk/blobimages/lcintl-lp/luk/images/grid/grid1.png" alt="" /></div>
@@ -89,9 +90,7 @@ const Home = () => {
                         </div>
                     </div>
 
-                </div>
-
-            <Footer marginTop="350px" />
+            <Footer marginTop="1%" />
                 
             </div>
            
